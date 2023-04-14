58586370381b3da65f0c6cd410a4ce878d567f9a
juanpc19 <juan.perez@iesnervion.es>
2023-04-14 14:31:24 +0200

    inicio main

--- Tema8Herencias-Ejercicios/src/ejercicio3/Main.java
+++ Tema8Herencias-Ejercicios/src/ejercicio3/Main.java
@@ -1,10 +1,28 @@
 package ejercicio3;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
 
+		String nombre = "";
+		
+		double precio;
+		
+		int diasCaducar;
+		
+		String tipo = "";
+		
+		int cantidad;
+
+		Producto lista[] = new Producto[10];
+
+		Scanner dogma = new Scanner(System.in);
+
+		//crud funciones
+		
+		dogma.close();
 	}
 
 }
